e46397267d89b27cbe73fe915420d8a4c544db48
xard1x <akuharenko21@gmail.com>
2025-05-21 15:01:21 +0500

    интерфейс и кнопка 1

--- calculator.py
+++ calculator.py
@@ -5,8 +5,146 @@ from PyQt5.QtGui import *
 app = QApplication([]) # создание окна
 win = QWidget()
 win.setWindowTitle('Калькулятор') 
-win.setMinimumSize(300, 500)
-win.setMaximumSize(300, 500)
+win.setMinimumSize(300, 350)
+win.setMaximumSize(300, 450)
 
+main_layout = QVBoxLayout()
+
+global label
+label = QLabel('')
+label.setFont(QFont('Arial', 30))
+lay1 = QHBoxLayout()
+lay1.addWidget(label, alignment= Qt.AlignLeft)
+main_layout.addLayout(lay1)
+
+but_proc = QPushButton('%')
+but_proc.setFont(QFont('Arial', 15))
+but_ce = QPushButton('CE')
+but_ce.setFont(QFont('Arial', 15))
+but_c = QPushButton('C')
+but_c.setFont(QFont('Arial', 15))
+but_del = QPushButton('DEL')
+but_del.setFont(QFont('Arial', 15))
+lay2 = QHBoxLayout()
+lay2.addWidget(but_proc)
+lay2.addWidget(but_ce)
+lay2.addWidget(but_c)
+lay2.addWidget(but_del)
+main_layout.addLayout(lay2)
+
+but_1x = QPushButton('1/x')
+but_1x.setFont(QFont('Arial', 15))
+but_x2 = QPushButton('x2')
+but_x2.setFont(QFont('Arial', 15))
+but_kor = QPushButton('√x')
+but_kor.setFont(QFont('Arial', 15))
+but_razdel = QPushButton('÷')
+but_razdel.setFont(QFont('Arial', 15))
+lay3 = QHBoxLayout()
+lay3.addWidget(but_1x)
+lay3.addWidget(but_x2)
+lay3.addWidget(but_kor)
+lay3.addWidget(but_razdel)
+main_layout.addLayout(lay3)
+
+but_7 = QPushButton('7')
+but_7.setFont(QFont('Arial', 15))
+but_8 = QPushButton('8')
+but_8.setFont(QFont('Arial', 15))
+but_9 = QPushButton('9')
+but_9.setFont(QFont('Arial', 15))
+but_umn = QPushButton('×')
+but_umn.setFont(QFont('Arial', 15))
+lay4 = QHBoxLayout()
+lay4.addWidget(but_7)
+lay4.addWidget(but_8)
+lay4.addWidget(but_9)
+lay4.addWidget(but_umn)
+main_layout.addLayout(lay4)
+
+but_4 = QPushButton('4')
+but_4.setFont(QFont('Arial', 15))
+but_5 = QPushButton('5')
+but_5.setFont(QFont('Arial', 15))
+but_6 = QPushButton('6')
+but_6.setFont(QFont('Arial', 15))
+but_minus = QPushButton('-')
+but_minus.setFont(QFont('Arial', 15))
+lay5 = QHBoxLayout()
+lay5.addWidget(but_4)
+lay5.addWidget(but_5)
+lay5.addWidget(but_6)
+lay5.addWidget(but_minus)
+main_layout.addLayout(lay5)
+
+but_1 = QPushButton('1')
+but_1.setFont(QFont('Arial', 15))
+but_2 = QPushButton('2')
+but_2.setFont(QFont('Arial', 15))
+but_3 = QPushButton('3')
+but_3.setFont(QFont('Arial', 15))
+but_plus = QPushButton('+')
+but_plus.setFont(QFont('Arial', 15))
+lay6 = QHBoxLayout()
+lay6.addWidget(but_1)
+lay6.addWidget(but_2)
+lay6.addWidget(but_3)
+lay6.addWidget(but_plus)
+main_layout.addLayout(lay6)
+
+but_sign = QPushButton('+/-')
+but_sign.setFont(QFont('Arial', 15))
+but_0 = QPushButton('0')
+but_0.setFont(QFont('Arial', 15))
+but_drob = QPushButton(',')
+but_drob.setFont(QFont('Arial', 15))
+but_ravno = QPushButton('=')
+but_ravno.setFont(QFont('Arial', 15))
+lay7 = QHBoxLayout()
+lay7.addWidget(but_sign)
+lay7.addWidget(but_0)
+lay7.addWidget(but_drob)
+lay7.addWidget(but_ravno)
+main_layout.addLayout(lay7)
+
+global num1
+num1 = '' 
+
+global num2
+num2 = '' 
+
+global phase
+phase = 2
+
+def one():
+    global phase
+    global label
+    global num1
+    global num2
+    if phase == 1:    
+        if num1 == '':
+            num1 = '1'
+            label.setText(str(num1))
+        else:
+            num1 += '1'
+            label.setText(str(num1))
+            if len(num1) >= 14:
+                num1 = num1[:-1]
+                print(num1)
+    if phase == 2:    
+        if num2 == '':
+            num2 = '1'
+            label.setText(str(num2))
+        else:
+            num2 += '1'
+            label.setText(str(num2))
+            if len(num2) >= 14:
+                num2 = num2[:-1]
+                print(num2)
+
+
+but_1.clicked.connect(one)
+
+win.setLayout(main_layout)
 win.show()
 app.exec()
